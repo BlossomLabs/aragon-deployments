f50230cb2bd2d47abc09c7575ee001862cb5e092
0xGabi <gabrielpk.18@gmail.com>
2023-08-02 14:20:38 -0300

    chore: remove existing deployments

--- scripts/helpers/environments.js
+++ scripts/helpers/environments.js
@@ -2,33 +2,5 @@ module.exports = {
   'local': {
     ens: '0x5f6f7e8cc7346a11ca2def8f827b7a0b612c56a1',
     node: 'http://localhost:8545'
-  },
-  'mainnet': {
-    ens: '0x314159265dd8dbb310642f98f50c066173c1259b',
-    node: 'https://mainnet.eth.aragon.network',
-  },
-  'rinkeby-old': {
-    ens: '0xfbae32d1cde62858bc45f51efc8cc4fa1415447e',
-    node: 'https://rinkeby.eth.aragon.network',
-  },
-  'rinkeby': {
-    ens: '0x98Df287B6C145399Aaa709692c8D308357bC085D',
-    node: 'https://rinkeby.eth.aragon.network',
-  },
-  'staging': {
-    ens: '0xfe03625ea880a8cba336f9b5ad6e15b0a3b5a939',
-    node: 'https://rinkeby.eth.aragon.network',
-  },
-  'ropsten': {
-    ens: '0x6afe2cacee211ea9179992f89dc61ff25c61e923',
-    node: 'https://ropsten.eth.aragon.network',
-  },
-  'mumbai': {
-    ens: '0x431f0eed904590b176f9ff8c36a1c4ff0ee9b982',
-    node: 'https://matic-testnet-archive-rpc.bwarelabs.com',
-  },
-  'matic': {
-    ens: '0x3c70a0190d09f34519e6e218364451add21b7d4b',
-    node: 'https://dry-old-leaf.matic.quiknode.pro',
   }
 }
