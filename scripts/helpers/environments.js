0662e854c36e6f51b1f2864035bea9180b1fba97
0xGabi <gabrielpk.18@gmail.com>
2023-10-28 02:27:04 -0300

    feat: add optimism environment metadata

--- scripts/helpers/environments.js
+++ scripts/helpers/environments.js
@@ -2,5 +2,9 @@ module.exports = {
   'local': {
     ens: '0x5f6f7e8cc7346a11ca2def8f827b7a0b612c56a1',
     node: 'http://localhost:8545'
+  },
+  'optimism': {
+    ens: '0x6f2CA655f58d5fb94A08460aC19A552EB19909FD',
+    node: 'https://optimism.publicnode.com'
   }
 }
